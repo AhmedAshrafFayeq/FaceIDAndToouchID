e8fea9dff951a3978ad35714039313caef0aad83
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-02-05 17:48:50 +0200

    setup View

--- Biometrics/Biometrics/ViewController.swift
+++ Biometrics/Biometrics/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let authButton    = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        authButton.center = view.center
+        authButton.setTitle("Authenticate", for: .normal)
+        authButton.backgroundColor = .systemGreen
+        authButton.addTarget(self, action: #selector(authenticate), for: .touchUpInside)
+        view.addSubview(authButton)
+    }
+    
+    @objc func authenticate(){
+        
     }
 
 
