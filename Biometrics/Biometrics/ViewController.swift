71aeeac4c8f844c19b3ba9db020f0fa2937a23de
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-02-05 19:22:43 +0200

    add biometric feature

--- Biometrics/Biometrics/ViewController.swift
+++ Biometrics/Biometrics/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
@@ -14,15 +15,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let authButton    = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(authButton)
         authButton.center = view.center
         authButton.setTitle("Authenticate", for: .normal)
         authButton.backgroundColor = .systemGreen
         authButton.addTarget(self, action: #selector(authenticate), for: .touchUpInside)
-        view.addSubview(authButton)
     }
     
     @objc func authenticate(){
-        
+        let context = LAContext()
+        var error: NSError? = nil
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "Please authorize"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, error in
+                guard let self = self else {return}
+                DispatchQueue.main.async {
+                    guard success, error == nil else{
+                        //failed
+                        let alert = UIAlertController(title: "Failed to Authenticate", message: "please try again", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                        self.present(alert, animated: true)
+                        return
+                    }
+                    //show another screen
+                    //success
+                    let vc = UIViewController()
+                    vc.title = "Welcome"
+                    vc.view.backgroundColor = .systemBlue
+                    self.present(UINavigationController(rootViewController: vc) , animated: true)
+                }
+            }
+        }else{
+            //can not use the feature
+            let alert = UIAlertController(title: "Unavailable", message: "you can not use this feature", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(alert, animated: true)
+        }
     }
 
 
