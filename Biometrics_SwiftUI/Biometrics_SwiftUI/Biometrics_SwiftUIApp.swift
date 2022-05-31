6eea9381a86e833188b5c541f8af9f9e973a3dca
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-31 22:11:31 +0200

    edit view

---
+++ Biometrics_SwiftUI/Biometrics_SwiftUI/Biometrics_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biometrics_SwiftUIApp.swift
+//  Biometrics_SwiftUI
+//
+//  Created by Ahmed Fayeq on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Biometrics_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
